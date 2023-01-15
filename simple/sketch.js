dd493be57dc997c2c62f3f032fc417929b1ddfc0
Katsuya Endoh <studio@enkatsu.org>
2023-01-16 02:15:45 +0900

    update simple

--- simple/sketch.js
+++ simple/sketch.js
@@ -7,11 +7,7 @@ let greenValue = 0;
 
 function setup() {
   createCanvas(300, 300);
-  hostNameInput = createInput('localhost');
-  portNumberInput = createInput(8081, 'number');
-  updateSerialConnectionButton = createButton('update serial connection');
-  updateSerialConnectionButton.mousePressed(updateSerialConnection);
-  portSelect = createSelect();
+  initUi();
   updateSerialConnection();
 }
 
@@ -35,25 +31,32 @@ function touchMoved() {
   }
 }
 
+function initUi() {
+  hostNameInput = createInput('localhost');
+  portNumberInput = createInput(8081, 'number');
+  updateSerialConnectionButton = createButton('update serial connection');
+  updateSerialConnectionButton.mousePressed(updateSerialConnection);
+  portSelect = createSelect();
+  portSelect.changed(function() {
+    serial.openPort(this.selected());
+  });
+}
+
 function updateSerialConnection() {
   if (serial && serial.isConnected()) {
     serial.closePort();
   }
 
   serial = new p5.SerialPort(hostNameInput.value(), portNumberInput.value());
-  serial.list();
   serial.on('list', gotList);
   serial.on('data', gotData);
+  serial.list();
 }
 
 function gotList(portList) {
   for (let port of portList) {
     portSelect.option(port);
   }
-
-  portSelect.changed(function() {
-    serial.openPort(this.selected());
-  });
 
   serial.openPort(portSelect.selected());
 }

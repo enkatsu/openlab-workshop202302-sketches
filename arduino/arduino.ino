fff002b4521b8f2c1b72300803d47accea86d70c
Katsuya Endoh <studio@enkatsu.org>
2023-01-19 14:12:15 +0900

    add arduino code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,4 +1,3 @@
-#include <MsTimer2.h>
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
 #include <avr/power.h>
@@ -8,28 +7,15 @@
 #define NUMPIXELS 10
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 50
-
 int byteIndex = 0;
 byte bytes[4] = {0, 0, 0, 0};
 
-void write_color() {
-  pixels.show();
-}
-
 void setup() {
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
   pixels.begin();
-  delay(1000);
   pixels.clear();
   pixels.show();
 
   Serial.begin(9600);
-
-  MsTimer2::set(100, write_color);
-  MsTimer2::start();
 }
 
 void loop() {
@@ -49,4 +35,5 @@ void loop() {
   uint8_t g = int(buf[2]);
   uint8_t b = int(buf[3]);
   pixels.setPixelColor(i, pixels.Color(r, g, b));
+  pixels.show();
 }

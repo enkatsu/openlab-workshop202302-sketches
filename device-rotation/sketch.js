55757535b8708c500123271590adbebc49349cfd
Katsuya Endoh <studio@enkatsu.org>
2023-01-19 14:11:53 +0900

    update structure

--- device-rotation/sketch.js
+++ device-rotation/sketch.js
@@ -1,13 +1,8 @@
-let serial;
-let portSelect;
 const pixelNum = 10;
 
 function setup() {
   createCanvas(300, 300);
-  serial = new p5.SerialPort();
-  serial.list();
-  serial.on('list', gotList);
-  serial.on('data', gotData);
+  initUi();
 }
 
 function draw() {
@@ -23,32 +18,8 @@ function draw() {
     const y = sin(angle) * rad + height / 2;
     const d = dist(x, y, rx, ry);
     ellipse(x, y, d * 0.1, d * 0.1);
-    sendPixelData(i, d, d, d);
+    if (serial) {
+      sendPixelData(i, d, d, d);
+    }
   }
-}
-
-function gotList(portList) {
-  portSelect = createSelect();
-  for (let port of portList) {
-    portSelect.option(port);
-  }
-
-  portSelect.changed(function() {
-    serial.openPort(this.selected());
-  });
-
-  serial.openPort(portSelect.selected());
-}
-
-function gotData() {
-  let currentString = serial.readLine();
-  console.log(currentString);
-}
-
-function sendPixelData(i, r, g, b) {
-  serial.write(parseInt(i));
-  serial.write(parseInt(r));
-  serial.write(parseInt(g));
-  serial.write(parseInt(b));
-  serial.write('\n');
 }

42c5717429b29ff3e5613b4f04fc6a53b552db51
Katsuya Endoh <studio@enkatsu.org>
2023-01-19 14:06:54 +0900

    update structure

--- fire/sketch.js
+++ fire/sketch.js
@@ -1,15 +1,8 @@
-let serial;
-let portSelect;
-let redValue = 0;
-let greenValue = 0;
 const pixelNum = 10;
 
 function setup() {
   createCanvas(300, 300);
-  serial = new p5.SerialPort();
-  serial.list();
-  serial.on('list', gotList);
-  serial.on('data', gotData);
+  initUi();
 }
 
 function draw() {
@@ -22,7 +15,9 @@ function draw() {
     const x = cos(rad) *  radius * n + width / 2;
     const y = sin(rad) *  radius * n + height / 2
     const c = fireColor(n);
-    sendPixelData(i, red(c), green(c), blue(c));
+    if (serial) {
+      sendPixelData(i, red(c), green(c), blue(c));
+    }    
     fill(c);
     ellipse(x, y, 3, 3);
   }
@@ -50,30 +45,4 @@ function cyclicNoise(rad, t) {
     xInit + rBase * cos(rad) * 0.01 * t,
     yInit + rBase * sin(rad) * 0.01 * t,
   );
-}
-
-function gotList(portList) {
-  portSelect = createSelect();
-  for (let port of portList) {
-    portSelect.option(port);
-  }
-
-  portSelect.changed(function() {
-    serial.openPort(this.selected());
-  });
-
-  serial.openPort(portSelect.selected());
-}
-
-function gotData() {
-  let currentString = serial.readLine();
-  console.log(currentString);
-}
-
-function sendPixelData(i, r, g, b) {
-  serial.write(parseInt(i));
-  serial.write(parseInt(r));
-  serial.write(parseInt(g));
-  serial.write(parseInt(b));
-  serial.write('\n');
 }

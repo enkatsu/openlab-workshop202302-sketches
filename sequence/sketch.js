fc0625369562ab270330f49de13f91f905342ad7
Katsuya Endoh <studio@enkatsu.org>
2023-01-21 20:03:33 +0900

    update sequence

--- sequence/sketch.js
+++ sequence/sketch.js
@@ -1,6 +1,14 @@
-let redValue = 0;
-let greenValue = 0;
-
+const sequence = [
+  [true, false, false, false, false, false, false, false, true, false, ],
+  [false, true, false, false, false, false, false, false, false, true, ],
+  [false, false, true, false, false, false, false, false, false, false, ],
+  [false, false, false, true, false, false, false, false, false, false, ],
+  [false, false, false, false, true, false, false, false, false, false, ],
+  [false, false, false, false, false, true, false, false, false, false, ],
+  [false, false, false, false, false, false, true, false, false, false, ],
+  [false, false, false, false, false, false, false, true, false, false, ],
+];
+let current = 0;
 
 function setup() {
   createCanvas(300, 300);
@@ -8,25 +16,50 @@ function setup() {
 }
 
 function draw() {
-  background(redValue, greenValue, 0);
-}
+  background(0);
 
-function mouseMoved() {
-  if (serial) {
-    redValue = parseInt(map(mouseX, 0, width, 0, 255));
-    greenValue = parseInt(map(mouseY, 0, height, 0, 255));
-    for (let i = 0; i < pixelNum; i++) {
-      sendPixelData(i, redValue, greenValue, 0);
+  for (let i = 0; i < sequence.length; i++) {
+    const pixels = sequence[i];
+    for (let j = 0; j < pixels.length; j++) {
+      if (i === current) {
+        stroke(255, 0, 0);
+      } else {
+        stroke(100);
+      }
+      if (pixels[j]) {
+        fill(255);
+      } else {
+        fill(0);
+      }
+
+      const w = width / sequence.length;
+      const h = height / pixels.length;
+      rect(w * i + 1, h * j + 1, w - 1, h - 1);
+    }
+  }
+
+  if (frameCount % 30 === 0) {
+    current++;
+    if (current >= sequence.length) {
+      current = 0;
+    }
+
+    if (serial) {
+      for (let i = 0; i < sequence[current].length; i++) {
+        if (sequence[current][i]) {
+          sendPixelData(i, 255, 255, 255);
+        } else {
+          sendPixelData(i, 0, 0, 0);
+        }
+      }
     }
   }
 }
 
-function touchMoved() {
-  if (serial) {
-    redValue = map(mouseX, 0, width, 0, 255);
-    greenValue = map(mouseY, 0, height, 0, 255);
-    for (let i = 0; i < pixelNum; i++) {
-      sendPixelData(i, redValue, greenValue, 0);
-    }
-  }
+function mousePressed() {
+  if (0 > mouseX || mouseX > width || 0 > mouseY || mouseY > height) return;
+
+  const x = parseInt(map(mouseX, 0, width, 0, sequence.length));
+  const y = parseInt(map(mouseY, 0, height, 0, sequence[x].length));
+  sequence[x][y] = !sequence[x][y];
 }

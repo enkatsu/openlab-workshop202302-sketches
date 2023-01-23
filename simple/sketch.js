176400ba896b3851c1ebcf2943e2bad6c46177c4
Katsuya Endoh <studio@enkatsu.org>
2023-01-23 17:59:14 +0900

    update serial protocol

--- simple/sketch.js
+++ simple/sketch.js
@@ -17,7 +17,8 @@ function mouseMoved() {
     redValue = parseInt(map(mouseX, 0, width, 0, 255));
     greenValue = parseInt(map(mouseY, 0, height, 0, 255));
     for (let i = 0; i < pixelNum; i++) {
-      sendPixelData(i, redValue, greenValue, 0);
+      const isLast = i === (pixelNum - 1);
+      sendPixelData(i, redValue, greenValue, 0, isLast ? 1 : 0);
     }
   }
 }
@@ -27,7 +28,8 @@ function touchMoved() {
     redValue = map(mouseX, 0, width, 0, 255);
     greenValue = map(mouseY, 0, height, 0, 255);
     for (let i = 0; i < pixelNum; i++) {
-      sendPixelData(i, redValue, greenValue, 0);
+      const isLast = i === (pixelNum - 1);
+      sendPixelData(i, redValue, greenValue, 0, isLast ? 1 : 0);
     }
   }
 }

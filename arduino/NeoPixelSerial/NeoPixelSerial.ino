eef9f1aef3eee21f443971177f55ea1e4816ce54
Katsuya Endoh <studio@enkatsu.org>
2023-01-23 17:58:16 +0900

    update serial protocol

--- arduino/NeoPixelSerial/NeoPixelSerial.ino
+++ arduino/NeoPixelSerial/NeoPixelSerial.ino
@@ -7,13 +7,11 @@
 #define NUMPIXELS 10
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int byteIndex = 0;
-byte bytes[4] = {0, 0, 0, 0};
-
 void setup() {
   pixels.begin();
   pixels.clear();
   pixels.show();
+//  pixels.setBrightness(50);
 
   Serial.begin(9600);
 }
@@ -24,16 +22,20 @@ void loop() {
   }
 
   String input = Serial.readStringUntil('\n');
-  if (input.length() != 4) {
+  if (input.length() != 5) {
     return;
   }
-  
-  byte buf[4] = {0, 0, 0, 0};
-  input.getBytes(buf, 4);
+
+  byte buf[5] = {0, 0, 0, 0, 0};
+  input.getBytes(buf, 5);
   uint16_t i = int(buf[0]);
   uint8_t r = int(buf[1]);
   uint8_t g = int(buf[2]);
   uint8_t b = int(buf[3]);
+  byte command = buf[4];
   pixels.setPixelColor(i, pixels.Color(r, g, b));
   pixels.show();
+  if (command == 1) {
+    pixels.show();
+  }
 }
